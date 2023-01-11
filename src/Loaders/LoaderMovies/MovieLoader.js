bf09c067dd1eb4683af05b72b6abb93dc302d029
Alesha Tikhonov <zukko961@gmail.com>
2023-01-11 12:15:36 +0300

    fix movie loader

--- src/Loaders/LoaderMovies/MovieLoader.js
+++ src/Loaders/LoaderMovies/MovieLoader.js
@@ -1,64 +1,15 @@
 import React from 'react'
 
 export default function MovieLoader() {
+   const items =  [...new Array(20)]
   return (
     <div className="w-full relative flex items-center justify-center">
     <div className="flex overflow-x-hidden animate-[preloader_1.5s_ease-in-out_infinite] p-2">
         <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-        </ul>
-        <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-        </ul>
-        <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-        </ul>
-        <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-        </ul>
-        <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-        </ul>
-        <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-        </ul>
-        <ul className='flex mb-28 lit:mb-14'>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
-                <li className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
-                    <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
-                </li>
+                {items.map((_, index) =>  
+                    <li key={index} className='bg-[#80808051] w-[160px] h-[160px] flex mr-6 rounded-sm relative justify-center items-center lit:w-24 lit:h-24'>
+                        <span className='absolute top-[100%] w-10 h-2 bg-[#80808051] mt-3 rounded-sm'></span>
+                    </li>)}
         </ul>
     </div>
 </div>
